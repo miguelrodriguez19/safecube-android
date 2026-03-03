618421bf28c62ed4458ce90945c4d114025249bc
Miguel Rodriguez <rodriguezherreromiguel@gmail.com>
2026-03-03 13:53:03 +0100

    feat(core:crypto): implement FakeCryptoEngine and configure Hilt bindings
    - Implement `FakeCryptoEngine` as a placeholder for encryption and decryption operations, returning data unmodified.
    - Configure `CryptoModule` to bind `FakeCryptoEngine` as the singleton implementation for the `CryptoEngine` interface.
    - Add necessary Dagger Hilt annotations for dependency injection and singleton lifecycle management.

--- core/crypto/src/main/java/com/miguelrodriguez19/safecube/core/crypto/di/CryptoModule.kt
+++ core/crypto/src/main/java/com/miguelrodriguez19/safecube/core/crypto/di/CryptoModule.kt
@@ -1,9 +1,20 @@
 package com.miguelrodriguez19.safecube.core.crypto.di
 
+import com.miguelrodriguez19.safecube.core.crypto.CryptoEngine
+import com.miguelrodriguez19.safecube.core.crypto.internal.FakeCryptoEngine
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object CryptoModule
+abstract class CryptoModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCryptoEngine(
+        fakeCryptoEngine: FakeCryptoEngine,
+    ): CryptoEngine
+}
